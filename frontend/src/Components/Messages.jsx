60ffd1e09db800a8e28b9c94336f06e22a781905
askor26 <askorutin26@gmail.com>
2022-07-21 21:59:25 +0300

    added messages component

---
+++ frontend/src/Components/Messages.jsx
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function Messages(props) {
+  const {
+    username,
+    channels,
+    currentChannel,
+    messages,
+    show,
+    handleClose,
+    handleShow,
+    socket,
+  } = props;
+
+  const channelMessages = messages.filter(
+    (message) => message.channel === currentChannel
+  );
+
+  return channelMessages.map((message) => {
+    const { name, id, channel } = message;
+
+    return (
+      <div className='text-break mb-2' id={id} key={id}>
+        <b>{username && username}</b>: {name}
+      </div>
+    );
+  });
+}
+export default Messages;
